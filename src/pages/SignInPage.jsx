9bc8e89567a4e1ae78d982c3ca6d339a18a5ea0f
Naomi <naomistrall@hotmail.com>
2023-07-05 14:29:47 -0300

    Primeiro commit: Inicialização

---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import MyWalletLogo from "../components/MyWalletLogo";
+
+export default function SignInPage() {
+  return (
+    <SingInContainer>
+      <form>
+        <MyWalletLogo />
+        <input placeholder="E-mail" type="email" autoComplete="email" />
+        <input placeholder="Senha" type="password" autoComplete="new-password" />
+        <button>Entrar</button>
+      </form>
+
+      <Link>Primeira vez? Cadastre-se!</Link>
+    </SingInContainer>
+  );
+}
+
+const SingInContainer = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
